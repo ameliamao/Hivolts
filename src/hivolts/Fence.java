c5173d5b56086f7216f2ca8df384bfbc40d537fc
Amelia <mao.amelia@gmail.com>
2017-09-28 11:02:29 -0700

    copied fence code

--- src/hivolts/Fence.java
+++ src/hivolts/Fence.java
@@ -1,5 +1,21 @@
 package hivolts;
+import java.awt.*;
 
 public class Fence {
-
+	public final int width = 100;
+	public final int height = 100;
+	private double x;
+	private double y;
+	private final Color color = Color.ORANGE;
+	
+	public Fence(double x, double y){
+		this.x = x;
+		this.y = height;
+		
+	}
+	public void draw (Graphics g){
+		g.setColor(this.color);
+		g.fillRect((int)x, (int)y, (int)width, (int)height);
+	}
+	
 }
