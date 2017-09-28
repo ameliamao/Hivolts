105053b0af5c7d65cfc9fa8ea6eacbb8b933d07c
Sophia Vera <sv37469@pausd.us>
2017-09-28 11:26:06 -0700

    added square, this can be changed later

--- src/hivolts/Square.java
+++ src/hivolts/Square.java
@@ -1,5 +1,16 @@
 package hivolts;
+import java.util.Random;
 
-public class Square {
+public abstract class Square {
+	int x;
+	int y;
+	int xCoordinate;
+	int yCoordinate;
+	
+	public void random(){
+		Random random = new Random();
+		x = random.nextInt(12);
+		y = random.nextInt(12);
+	}
 
 }
